e357b1f23f8f6890fafa78734cb26335bb5cc8b9
Joel McCracken <mccracken.joel@gmail.com>
2015-12-22 21:34:18 -0500

    remove extra pry, promote config to outer module

--- lib/funstation/context.rb
+++ lib/funstation/context.rb
@@ -1,18 +1,14 @@
 module Funstation
   class Context
     def config
-      require 'yaml'
-      require 'pry'
-      binding.pry
       init_path = File.expand_path "~/.funstation.d/init.rb"
       content = File.read(init_path)
       config = Config.new
       config.instance_eval(content, init_path, 1)
       config
     end
-
-    class Config
-      attr_accessor :config
-    end
+  end
+  class Config
+    attr_accessor :config
   end
 end
