46c038ac5e3ac86c51d15e0cb1f04c7a04f7de7e
Joel McCracken <mccracken.joel@gmail.com>
2015-12-31 14:41:12 -0500

    add code for module registration

--- lib/funstation.rb
+++ lib/funstation.rb
@@ -1,8 +1,17 @@
+module Funstation
+  def self.register_module(name, mod)
+    @registered_modules ||= {}
+    @registered_modules[name] = mod
+  end
+  def self.registered_modules
+    @registered_modules
+  end
+end
+
 require 'funstation/context'
 require 'funstation/root_cli'
 require 'funstation/sysadmin'
 require 'funstation/dirt_alert'
 require 'funstation/handle_inbox'
-
-module Funstation
-end
+require 'funstation/git_home_dir'
+require 'funstation/polite_git_checkout'
--- lib/funstation/context.rb
+++ lib/funstation/context.rb
@@ -1,14 +1,44 @@
 module Funstation
   class Context
+    attr_reader :registered_modules
+
+    def initialize(registered_modules)
+      @registered_modules = registered_modules
+    end
+
     def config
-      init_path = File.expand_path "~/.funstation.d/init.rb"
-      content = File.read(init_path)
-      config = Config.new
-      config.instance_eval(content, init_path, 1)
-      config
+      @config ||=
+        begin
+          init_path = File.expand_path("~/.funstation.d/init.rb")
+          content = File.read(init_path)
+          config = Config.new
+          config.instance_eval(content, init_path, 1)
+          config
+        end
+    end
+
+    def each_module
+      config.modules.each do |mod|
+        the_mod = registered_modules[mod]
+
+        if the_mod
+          yield the_mod
+        else
+          $stderr.puts "Unknown module listed in configuration file #{mod}"
+        end
+      end
     end
   end
+
   class Config
     attr_accessor :config
+
+    def [](x)
+      config[x]
+    end
+
+    def modules
+      config[:modules]
+    end
   end
 end
