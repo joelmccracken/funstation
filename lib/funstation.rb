d2faf2879336c2fc3652d32d61112a23a2259d01
Joel McCracken <mccracken.joel@gmail.com>
2016-02-06 23:25:18 -0500

    restore code lost when gemifying

--- lib/funstation.rb
+++ lib/funstation.rb
@@ -1,5 +1,20 @@
 require "funstation/version"
 
 module Funstation
-  # Your code goes here...
+  def self.register_module(name, mod)
+    @registered_modules ||= {}
+    @registered_modules[name] = mod
+  end
+  def self.registered_modules
+    @registered_modules
+  end
 end
+
+require 'funstation/context'
+require 'funstation/root_cli'
+require 'funstation/sysadmin'
+require 'funstation/dirt_alert'
+require 'funstation/handle_inbox'
+require 'funstation/git_home_dir'
+require 'funstation/daemon'
+require 'funstation/polite_git_checkout'
