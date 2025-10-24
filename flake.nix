75f878248151e05d548ed76c034e8a6e1d156b2e
Joel McCracken <mccracken.joel@gmail.com>
2025-10-23 21:15:38 -0400

    working build

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
           helloProject =
             final.haskell-nix.project' {
               src = ./.;
-              compiler-nix-name = "ghc910";
+              compiler-nix-name = "ghc9122";
               # This is used by `nix develop .` to open a shell for use with
               # `cabal`, `hlint` and `haskell-language-server`
               shell.tools = {
