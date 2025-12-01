9c3eeda92b0650aca68a859094849d7649a016fd
Joel McCracken <mccracken.joel@gmail.com>
2025-11-30 20:07:41 -0500

    start tests

--- src/WSHS/Types/Configuration.hs
+++ src/WSHS/Types/Configuration.hs
@@ -1,14 +1,29 @@
 {-# LANGUAGE DeriveAnyClass    #-}
-
+{-# LANGUAGE TemplateHaskell #-}
 module WSHS.Types.Configuration where
 
 import Data.Text (Text)
-import Data.Yaml (FromJSON)
+import Data.Yaml (FromJSON, Value)
+import Data.Aeson.Types
+import Data.Aeson.TH
 import GHC.Generics (Generic)
+
+data GitHomeDirPS = GitHomeDirPS { gitDir :: Text }
+ deriving Show
+
+$(deriveJSON defaultOptions ''GitHomeDirPS)
+
+data PropSettings
+  = GitHomeDir GitHomeDirPS
+  deriving (Show)
+
+deriveJSON defaultOptions{ sumEncoding = TaggedObject {tagFieldName = "type", contentsFieldName = "params" }
+                           } ''PropSettings
 
 data Configuration = Configuration
   { dotfilesRepoUrl :: Text
   , dotfilesRepoOrigin :: Text
   , workstationName :: Text
+  , properties :: [PropSettings]
   }
   deriving (Generic, Show, FromJSON)
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,60 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main (main) where
 
+-- file Spec.hs
+import Test.Hspec
+import Control.Exception (evaluate)
+import Text.RawString.QQ
+import Data.Text
+import Data.Text.Encoding (encodeUtf8)
+import Data.Yaml (decodeThrow)
+import WSHS.Types.Configuration
+import Control.Monad (-- void,
+                      forM_)
+
+
+
+
+  -- - type: Dotfiles
+  --   src-dir: dotfiles/
+  --   files:
+  --   - type: ln dot
+  --     name: bashrc
+
+  --   - type: ln dot
+  --     name: gitconfig
+
+  --   - type: ln dot
+  --     name: zshrc
+
+  --   - type: ln
+  --     name: Brewfile
+
+  --   - type: ln dot dir
+  --     name: config/git
+
+configText :: Text
+configText = [r|
+dotfilesRepoUrl: "https://github.com/joelmccracken/dotfiles"
+dotfilesRepoOrigin: "git@github.com:joelmccracken/dotfiles.git"
+workstationName: "aeglos"
+properties:
+- type: GitHomeDir
+  gitDir: ".git-homedir"
+|]
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec $ do
+  describe "configuration" $ do
+    it "extracts configuration for properties" $ do
+      cfg <- decodeThrow (encodeUtf8 configText) :: IO Configuration
+      forM_ cfg.properties $ \prop -> do
+        print prop
+
+      -- withPropCfg "git-home-dir" Nothing (\x -> pure $ Just ("" :: Text))
+
+      'a' `shouldBe` 'a'
