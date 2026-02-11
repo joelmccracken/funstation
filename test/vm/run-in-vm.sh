814351fc5d82757863e3008ca1a50f9d7377201d
Joel McCracken <mccracken.joel@gmail.com>
2026-02-11 13:44:06 -0500

    prep for putting online

--- test/vm/run-in-vm.sh
+++ test/vm/run-in-vm.sh
@@ -1,4 +1,7 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+
+set -x
 
 STARTING_SNAPSHOT='Snapshot 4'
 
@@ -8,13 +11,20 @@ nix build
 export VMRUN="/Applications/VMware Fusion.app/Contents/Library/vmrun";
 export VMX="$HOME/Virtual Machines.localized/macOS 14.vmwarevm/macOS 14.vmx";
 export DIR="vmwareshared/run-in-vm-dir"
-export HOSTDIR="$HOME/DIR"
-export GUESTDIR="/Volumes/VMWare\\\ Shared\\\ Folders/$DIR"
+export HOSTDIR="$HOME/$DIR"
 mkdir -p "$HOSTDIR"
-for f in $HOSTDIR/*; do rm -f $f; done
-cp ./result/bin/wshs test/sample_configs/just_nix.yml "$DIR"
+for f in $HOSTDIR/*; do rm -f $f; done # bypassing that wshs ends up being write protected, there must be a better way to do this
+cp ./result/bin/wshs test/sample_configs/just_nix.yml "$HOSTDIR"
 
 "$VMRUN" -T fusion revertToSnapshot "$VMX" "$STARTING_SNAPSHOT";
 "$VMRUN" -T fusion start "$VMX" gui;
 "$VMRUN" -T fusion -gu joel -gp password runScriptInGuest "$VMX" -interactive /bin/bash \
-    "osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp '$GUESTDIR'/* .; ./wshs --nopasswd bootstrap just_nix.yml aeglos\"'"
+    "set -x; osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/run-in-vm-dir/* .; ./wshs --nopasswd bootstrap just_nix.yml aeglos\"'"
+
+
+# TODO come up with some way to get the output to the cli (optionally) for claude to use
+
+
+# -"$VMRUN" -T fusion -gu joel -gp password runScriptInGuest "$VMX" -interactive /bin/bash "osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/* .; ./wshs bootstrap just_nix.yml aeglos\"'"
+# +"$VMRUN" -T fusion -gu joel -gp password runScriptInGuest "$VMX" -interactive /bin/bash \
+# +    "osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/* .; ./wshs bootstrap just_nix.yml aeglos\"'"
