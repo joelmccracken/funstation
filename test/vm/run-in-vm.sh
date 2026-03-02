cde193318f7fbd1aa3548adefbf746529f14d809
Joel McCracken <mccracken.joel@gmail.com>
2026-03-01 19:44:26 -0500

    clean up run-in-vm script

--- test/vm/run-in-vm.sh
+++ test/vm/run-in-vm.sh
@@ -26,18 +26,3 @@ echo password > "$HOSTDIR"/sudo-pass
 "$VMRUN" -T fusion -gu user -gp password runScriptInGuest "$VMX" -interactive /bin/bash \
     "set -x; osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/run-in-vm-dir/* .; ./wshs --sudo-cache --sudo-pass-file ./sudo-pass bootstrap just_nix.yml aeglos\"'"
 
-
-# TODO come up with some way to get the output to the cli (optionally) for claude to use
-
-
-# -"$VMRUN" -T fusion -gu joel -gp password runScriptInGuest "$VMX" -interactive /bin/bash "osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/* .; ./wshs bootstrap just_nix.yml aeglos\"'"
-# +"$VMRUN" -T fusion -gu joel -gp password runScriptInGuest "$VMX" -interactive /bin/bash \
-# +    "osascript -e 'tell app \"Terminal\" to do script \"mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\\ Shared\\\ Folders/vmwareshared/* .; ./wshs bootstrap just_nix.yml aeglos\"'"
-
-
-
-
-
-# '/Applications/VMware Fusion.app/Contents/Library/vmrun' -T fusion revertToSnapshot '/Users/joelmccracken/Virtual Machines.localized/macOS_14.vmwarevm/macOS_14.vmx' after-allow-vmware-tools-permissions
-# '/Applications/VMware Fusion.app/Contents/Library/vmrun' -T fusion start '/Users/joelmccracken/Virtual Machines.localized/macOS_14.vmwarevm/macOS_14.vmx' gui
-# '/Applications/VMware Fusion.app/Contents/Library/vmrun' -T fusion -gu user -gp password runScriptInGuest '/Users/joelmccracken/Virtual Machines.localized/macOS_14.vmwarevm/macOS_14.vmx' -interactive /bin/bash 'set -x; osascript -e '\''tell app "Terminal" to do script "mkdir -p ~/testing; cd ~/testing; cp /Volumes/VMWare\\ Shared\\ Folders/vmwareshared/run-in-vm-dir/* .; ./wshs --sudo-cache --sudo-pass-file ./sudo-pass bootstrap just_nix.yml aeglos"'\'''
