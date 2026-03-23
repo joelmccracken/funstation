4ae6c853dbad51e2d972277bd6f84a0453f11235
Joel McCracken <mccracken.joel@gmail.com>
2026-03-22 23:45:17 -0400

    missed module

---
+++ test/Util.hs
@@ -0,0 +1,14 @@
+module Util where
+
+import Test.Hspec
+import Data.Text qualified as T
+
+import WSHS.Types
+import WSHS.Properties.Git (GitHomeDirCloneP (..))
+
+
+
+failLeft result =
+  case result of
+    Left (WSFailure msg) -> fail $ "WS action failed: " <> T.unpack msg
+    Right a -> pure a
