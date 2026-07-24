01e751491e8626e1904f77ff6f62694d4b11059e
Joel McCracken <mccracken.joel@gmail.com>
2026-07-23 21:03:46 -0400

    remove old unused code in tests

--- test/Main.hs
+++ test/Main.hs
@@ -3,14 +3,9 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE ImpredicativeTypes #-}
 
-module Main (main, main2) where
+module Main (main) where
 
 import Test.Hspec
-import Text.RawString.QQ
-import Data.Text (Text)
-import Control.Monad.IO.Class
-import Shh.Internal (exe, captureTrim, (|>), tryFailure, toArgs, asArg, displayCommand)
-import Data.ByteString.Lazy hiding (writeFile, readFile, length)
 
 import qualified SudoSpec
 import qualified WorkstationSpec
@@ -18,43 +13,6 @@ import qualified UtilsSpec
 import qualified Properties.GitHomeDirSpec
 import qualified Properties.GitCloneSpec
 import qualified Properties.DotfilesSpec
-
-configText :: Text
-configText = [r|
-properties:
-- type: GitHomeDir
-  params:
-    gitDir: ".git-homedir"
-|]
-
-
-
-main2 :: IO ()
-main2 = hspec $ do
-  it "experimentation space" $ do
-    liftIO $ tryFailure $ toArgs (asArg "env") "touch" "foobar"
-    liftIO $ tryFailure $ exe ["env", "bash", "-c", "echo something great > foobar2"]
-    capt <- liftIO $ ( exe ["env", "cat", "foobar2"] |> captureTrim)
-
-    capt `shouldBe` "something great"
-
-    let pth = "foobar2"
-    foo <- liftIO $ do
-      let x = ["echo", "the file is '", capt, "' at filen named ", pth ]
-      useSudo <- pure False
-      putStrLn "before"
-      if useSudo
-        then pure ((exe $ "sudo" : x) :: [ByteString])
-        else pure (exe $ "env" : x)
-    putStrLn $ show $ displayCommand $ exe foo
-    putStrLn "before1"
-    _ <- exe foo
-    putStrLn "after1"
-
-    -- userCmd <- mkPrivCmd "sudo" WriteAccess "root_only" ["bash", "-c", "echo $USER"]
-    -- user <- liftIO $ userCmd |> captureTrim
-    -- -- TL.putStrLn $ TL.decodeLatin1 res
-    -- user `shouldBe` "root"
 
 main :: IO ()
 main = hspec $ do
